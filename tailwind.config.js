37a109e7a299568d1cd577a0fad670fa8bdee72c
vince03100 <vinceambro0310@gmail.com>
2026-03-03 15:33:08 +0100

    chore(setup): inizializzazione progetto Laravel + React + HeroUI

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+import { heroui } = require("@heroui/react");
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+        './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, heroui()],
+};
